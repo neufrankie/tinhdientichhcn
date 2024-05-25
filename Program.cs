5931bbf43ed1618cb73fa3142ad72fe2b15dff95
neufrankie <neu.vietanh272@gmail.com>
2024-05-25 20:02:56 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TinhDienTichHinhChuNhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nhap chieu rong tu ban phim
+            Console.Write("Nhap vao chieu rong cua hinh chu nhat: ");
+            double chieuRong = Convert.ToDouble(Console.ReadLine());
+
+            // Nhap chieu dai tu ban phim
+            Console.Write("Nhap vao chieu dai cua hinh chu nhat: ");
+            double chieuCao = Convert.ToDouble(Console.ReadLine());
+
+            // Tinh dien tich cua hinh chu nhat
+            double dienTich = chieuRong * chieuCao;
+
+            // Hien thi ket qua
+            Console.WriteLine("Dien tich cua hinh chu nhat do la: " + dienTich);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# tinhdientichhcn
+ 
